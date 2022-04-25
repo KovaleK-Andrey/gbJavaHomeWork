5613e2b43e6611d566fa0be9b45e9a460da1fe45
Andrey Kovalenko <andreykovalenko94@gmail.com>
2022-04-25 21:13:30 +1000

    create Main class + lesson 1

--- src/HomeWorkApp/Main.java
+++ src/HomeWorkApp/Main.java
@@ -3,6 +3,37 @@ package HomeWorkApp;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        printThreeWords();
+        checkSumSign(0, -10);
+        printColor(0);
+        compareNumbers(11, 11);
+
+    }
+
+    public static void  printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void  checkSumSign(int a, int b) {
+        String result = a + b >= 0 ? "Сумма положительная" : "Сумма отрицательная";
+        System.out.println(result);
+    }
+
+    public static void printColor(int a) {
+        if (a <= 0) {
+            System.out.println("Красный");
+        } else if (a > 0 && a <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers(int a, int b) {
+        String result = a >= b ? "a >= b" : "a < b";
+        System.out.println(result);
     }
 }
